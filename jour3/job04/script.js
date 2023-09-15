067aa3f37a4ad07584c8730c8d336200013b6fc5
fabien-ricca <fabien.ricca@laplateforme.io>
2023-09-15 12:43:30 +0200

    Jour3_job04

--- jour3/job04/script.js
+++ jour3/job04/script.js
@@ -3,10 +3,16 @@ function somme(a, b){
     return ((Number.isInteger(a) && Number.isInteger(b))) ? a + b : "Merci de ne saisir que des nombres.";
 }
 
-function strLen(str){
-    return (typeof str === "string") ? str.length : "Merci de ne saisir qu'une phrase.";
-}
+func3 = (str) => {
+    if(typeof str === "string"){
+        return str.length;
+    }
+    else{
+        return "Merci de ne saisir qu'une phrase.";
+    }
+};
 
 console.log(somme(3, 5));
 
-console.log(strLen("Bonjour"));
+console.log(func3("toto"));
+
